c08296b7e7325930068b0656213dfd763d90f780
群晖CentOS <1060778506@qq.com>
2026-08-31 16:29:18 +0800

    2026-8-31

--- fengjing_app/fengjing_business/doctype/temu___full_custody___completion_document/test_temu___full_custody___completion_document.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Fengjing E-Commerce and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTEMUFullCustodyCompletionDocument(FrappeTestCase):
-	pass
--- fengjing_app/fengjing_business/doctype/temu___full_custody___order___sub___table___sales_outstanding/temu___full_custody___order___sub___table___sales_outstanding.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Fengjing E-Commerce and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class TEMUFullCustodyOrderSubTableSalesoutstanding(Document):
-	pass
