ae17d662d968b2c5ea4a4fd5f2e3dce32cca9f03
群晖CentOS <1060778506@qq.com>
2026-09-03 17:18:50 +0800

    2026-8-31-1

---
+++ fengjing_app/fengjing_business/doctype/warehouse_operation_employee/warehouse_operation_employee.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Fengjing E-Commerce and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class WarehouseOperationEmployee(Document):
+    pass
