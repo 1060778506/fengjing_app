a50b84ded187d51683d9a695ce06c951cf952c29
群晖CentOS <1060778506@qq.com>
2026-09-15 19:56:58 +0800

    2026-8-31-1

---
+++ fengjing_app/fengjing_business/page/ozon_platform_overvi/ozon_platform_overvi.js
@@ -0,0 +1,7 @@
+frappe.pages['ozon-platform-overvi'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Ozon 平台概况',
+		single_column: true
+	});
+}
