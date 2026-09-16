ecfbc92dc8eb310b236cb98a41a487ba53ebfd61
群晖CentOS <1060778506@qq.com>
2026-09-16 18:12:19 +0800

    2026-9-15-1

---
+++ fengjing_app/fengjing_business/page/ozon_freight_calcula/ozon_freight_calcula.js
@@ -0,0 +1,7 @@
+frappe.pages['ozon-freight-calcula'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Ozon运费计算',
+		single_column: true
+	});
+}
